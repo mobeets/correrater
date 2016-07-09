1ddd595ab4a6b276fe24fbaef23a03b45cdee2b7
mobeets <mobeets@gmail.com>
2016-07-09 13:06:55 -0400

    more data

--- model/search.py
+++ model/search.py
@@ -29,7 +29,7 @@ def find_movie(name):
 
 def get_movie_ids():
     ids = []
-    for infile in glob.glob('data/*.txt'):
+    for infile in glob.glob('data/ids/*.txt'):
         ids.extend([line.strip() for line in open(infile).readlines()])
     return list(set(ids))
 
@@ -71,7 +71,7 @@ def stash_imdb_ids():
     urls = [TOP_URL, NEW_URL, POP_URL]
     for (nm, url) in zip(nms, urls):
         ids = fetch_movie_ids(url)
-        fnm = os.path.join('data', 'imdb_ids_' + nm + '.txt')
+        fnm = os.path.join('data', 'ids', 'imdb_ids_' + nm + '.txt')
         with open(fnm, 'w') as f:
             f.write('\n'.join(ids))
 
@@ -80,7 +80,7 @@ def stash_imdb_ids_from_files():
     for infile in glob.glob(indir):
         ids = fetch_movie_ids_from_file(infile)
         nm = os.path.splitext(os.path.split(infile)[-1])[0]
-        fnm = os.path.join('data', 'imdb_ids_' + nm + '.txt')
+        fnm = os.path.join('data', 'ids', 'imdb_ids_' + nm + '.txt')
         with open(fnm, 'w') as f:
             f.write('\n'.join(ids))
 
